39bdfb90e6f6b8eeea651c36d7f410c69bda583f
OurGitAccouynt <61327770+OurGitAccouynt@users.noreply.github.com>
2020-02-21 10:42:52 -0800

    Add files via upload

---
+++ Win_game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_game : MonoBehaviour
+{
+    public string level_name;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel(level_name);
+        }
+    }
+
+}
